af5c3f21aacc9c9cb5e3df479b9f91940cccecdf
mirzahammad1 <mirzahammadbaig147@gmail.com>
2023-08-09 00:33:20 +0500

    Point--Estimators

---
+++ 12__Point_Estimators.py
@@ -0,0 +1,6 @@
+# data of 40 employee random
+data = [18,99,20,95,78,80,96,30,49,63,66,29,96,38,39,93,52,43,14,83,
+        14,53,98,40,96,34,10,61,73,23,78,73,87,29,16,21,69,72,61,38]
+n = len(data)
+sample_mean = sum(data)/n
+print("Sample mean :  " , sample_mean)
